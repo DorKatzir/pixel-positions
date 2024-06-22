e3dd99b4302fe5a90492088ae3fb0835495648ab
Dor Katzir <katzirdor@gmail.com>
2024-06-22 23:49:15 +0300

    register page with forms components

---
+++ resources/views/auth/register.blade.php
@@ -0,0 +1,8 @@
+<x-layout>
+    <x-page-heading>Register</x-page-heading>
+
+    <x-forms.form method="POST" action="/register">
+        <x-forms.input lable="Your Name" name="name" />
+
+    </x-forms.form>
+</x-layout>
